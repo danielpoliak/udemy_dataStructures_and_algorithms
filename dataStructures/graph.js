d53d2236516611da1e24ac2c32df6560f64e3bee
Daniel Poliak <daniel.poliak@pfister.ch>
2021-03-02 22:02:50 +0100

    add depth first recursive iteration

--- dataStructures/graph.js
+++ dataStructures/graph.js
@@ -24,6 +24,25 @@ class Graph{
         }
         delete this.adjacencyList[vertex]
     }
+
+    depthFirstRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor)
+                }
+            });
+        })(start);
+
+        return result;
+    }
 }
 
 
